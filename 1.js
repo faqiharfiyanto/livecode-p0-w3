afc3a60d75fbae9f97c9e20c509a60ba73b43006
Faqih Arfiyanto <faqiharfiyanto@gmail.com>
2019-09-04 17:07:56 +0700

    upload nmr 1-3

---
+++ 1.js
@@ -0,0 +1,22 @@
+/*
+  ======================
+  Mencari yang terpintar
+  ======================
+  
+  Buatlah algoritma atau pseudocode untuk mencari semua angka diatas rata-rata.
+  Data yang diberikan berupa satu set angka dari nilai ujian murid-murid
+  di kelas.
+
+JAWAB
+rumus  rata2 = total dibagi banyak data
+contoh data:  70 60 50 40 30 80 90
+
+ALGORITMA
+  Buat data nilai para siswa
+  Buat variabel totalNilai dengan isinya yakni hasil penjumlahan seluruh nilai siswa
+  Buat variabel totalData dengan isinya yakni total banyaknya siswa
+  Buat variabel nilaiRata2 dengan isinya yakni hasil hitung dari totalNilai dibagi dengan totalData
+  Buat variabel hasil dengan dimulai tanpa isi
+  Apabila nilai siswa lebih besar dari nilaiRata2, maka masukkan nilai siswa tsb ke variabel hasil dengan tipe data string dan tanda pemisah koma (,) agar nilai tidak dijumlahkan oleh komputer atau menumpuk dan sulit dibaca.
+  Tampilkan variabel hasil
+*/
